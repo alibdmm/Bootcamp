66c3b45e51b71c6699dce24e93fa234bb36b909c
Ali Badem <alii.bademm@gmail.com>
2022-03-07 13:29:11 +0300

    update

--- SQL/Ödev 1-7.sql
+++ SQL/Ödev 1-7.sql
@@ -1,4 +1,4 @@
---********* ÖDEV1 *********** 
+--********* ÖDEV1 **********
 --select title, description from film;
 --select * from film where length > 60 and length < 75
 --select * from film where rental_rate =0.99 and (replacement_cost = 12.99 or replacement_cost = 28.99)
